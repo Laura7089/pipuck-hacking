e4a2bc43dd89ecdfb713b0621824862f38ec816d
Laura Demkowicz-Duffy <laura@laurademkowiczduffy.co.uk>
2022-02-22 03:11:52 +0000

    feat: all hostname_set script

---
+++ scripts/hostname_set.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+HOSTNAME_FILE="/etc/hostname"
+
+if [ -n "$(cat $HOSTNAME_FILE)" ]; then
+    echo "Hostname already set to $(cat $HOSTNAME_FILE)"
+    exit 0
+fi
+
+root_dev=$(mount | grep "on / " | cut -d " " -f 1)
+fs_uuid=$(lsblk -no UUID "$root_dev")
+uuid_part=$(echo "$fs_uuid" | cut -d "-" -f 2)
+
+hostname="pi-$uuid_part"
+echo "$hostname" > HOSTNAME_FILE
