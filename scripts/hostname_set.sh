79728af2b77db1f972f1cebf89fbcf81e1fe0429
Laura Demkowicz-Duffy <laura@laurademkowiczduffy.co.uk>
2022-02-24 16:46:00 +0000

    fix: use older hostnamectl syntax in hostname_set

--- scripts/hostname_set.sh
+++ scripts/hostname_set.sh
@@ -2,7 +2,7 @@
 set -ex
 
 HOSTNAME_FILE="/etc/hostname"
-CURRENT_HOSTNAME=$(hostnamectl hostname)
+CURRENT_HOSTNAME=$(cat $HOSTNAME_FILE || echo "")
 
 # If hostname is not "pi-puck" and not blank, don't change it
 if [ "$CURRENT_HOSTNAME" != "pi-puck" ] && [ -n "$CURRENT_HOSTNAME" ]; then
@@ -16,4 +16,4 @@ uuid_part=$(echo "$fs_uuid" | cut -d "-" -f 2)
 
 hostname="pi-$uuid_part"
 echo "Setting hostname to '$hostname'..."
-hostnamectl hostname "$hostname"
+hostnamectl set-hostname "$hostname"
